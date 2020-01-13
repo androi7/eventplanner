7f4d08cd91e97a6dd209442715d32932c4f64b7c
Gabriel Hilti <gabriel.hilti@gmail.com>
2020-01-13 15:08:36 +1100

    deploying to heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.persisted?
       redirect_to '/'
     else
       redirect_to '/signup'
@@ -23,5 +23,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true, length: { maximum: 50 }
-  validates :password_confirmation, presence: true
+  validates :password, presence: true
+  validates :password, confirmation: { case_sensitive: true }
 end
