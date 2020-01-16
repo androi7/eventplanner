b5108ecc56abd5a8961527ff54b38500edd3ec5f
Gabriel Hilti <gabriel.hilti@gmail.com>
2020-01-17 08:00:32 +1100

    deactivate google maps

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,12 @@
 class Event < ApplicationRecord
 
   # Tell geocoder where to look to get the human-readable address string
-  geocoded_by :address
+  #geocoded_by :address
 
   # Tell geocoder to actually turn the above address into a latitude
   # and longitude (by doing a lookup in a remote API)
   # any time you create or update a Event item
-  after_validation :geocode
+  #after_validation :geocode
 
   belongs_to :inspiration, optional: true
   belongs_to :category
